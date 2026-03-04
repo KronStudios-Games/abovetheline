5f3b019197e15f855a23e36e7287f5c96a7faac0
noobforlunch <daedalusgames123@gmail.com>
2026-03-04 02:13:53 -0600

    Upload Initial Block Out and Procedural Tile Generation Plugin (#16)
    * Upload Initial Block Out and Procedural Tile Generation Plugin
    
    This Commit adds the following to the Block Out Kit Prototype branch:
    Assets
    
    2 floor tile variations (Black and White)
    2 wooden door static meshes (damaged and clean) with proper pivots
    2 wooden door frame variations (damaged and clean)
    
    Custom procedural tile generation plugin for streamlined tile floor creation
    
    Block out level featuring:
    
    Basic room with 3 walls
    Tile generation actor showcase
    4 door placements with door frames
    
    * Delete ..txt
    
    * Delete ..txt
    
    * Delete ..txt
    
    ---------
    
    Co-authored-by: KronTheDev <165278219+KronTheDev@users.noreply.github.com>

---
+++ Plugins/TileFloorGenerator/Source/TileFloorGenerator/TileFloorGenerator.Build.cs
@@ -0,0 +1,16 @@
+using UnrealBuildTool;
+
+public class TileFloorGenerator : ModuleRules
+{
+	public TileFloorGenerator(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine"
+		});
+	}
+}
